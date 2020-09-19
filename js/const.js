5c2ba48b749afec6edba677011cb4cec7902e3d4
AbhishekPratap05 <pratap.5dec@gmail.com>
2020-09-19 16:10:30 +0530

    created game grid

---
+++ js/const.js
@@ -0,0 +1,12 @@
+//game constants 
+export const GRID_SIZE = 20;
+export const CELL_SIZE = 20;
+
+export const INITIAL_SCORE = 0;
+
+export const POWER_PILL_TIME = 10000;
+export const GLOBAL_SPEED = 80;
+
+//game labels
+export const WIN = 'WIN!';
+export const LOSE = 'GAME OVER!'
--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-console.log('Working!');
+import { LEVEL,OBJECT_TYPE  } from "./setup";
+import { INITIAL_SCORE, GLOBAL_SPEED, POWER_PILL_TIME} from "./const";
+import GameBoard from './GameBoard';
+
+//DOM Elements
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("start-button");
+
+const gameBoard = GameBoard.createGameBoard(gameGrid,LEVEL);
+
+//Initial Score
+let score = INITIAL_SCORE;
+let timer = null;
+let gameWin =false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+
+const gameOver = (packman,grid) => {
+    
+}
+
+const checkCollision = (packman,ghost) => {
+
+}
+
+const startGame = () => {
+
+}
