4b351663aebe000fbdc43640ceb42e8939e20a59
maxbebop <m.kubarsky@gmail.com>
2022-07-21 16:08:01 +0300

    js to ts

--- types.ts
+++ types.ts
@@ -1,9 +1,17 @@
-export type ObjectExistFun = (pos: number, object: string) => boolean;
-
-export type RandomMovementFun = (position: number, direction: any, objectExist: any) => any;
 
 export interface IDirection {
     code: number,
     movement: number,
     rotation: number
 }
+
+export interface IMovement {
+    nextMovePos: number,
+    direction: IDirection
+}
+
+
+export type ObjectExistFun = (pos: number, object: string) => boolean;
+
+export type RandomMovementFun = (position: number, direction: IDirection, objectExist: ObjectExistFun) => IMovement;
+
